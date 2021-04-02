dc9fd7c5c93057136b784e065da236bcb8ad801a
Wydzials <szymonstefan1999@gmail.com>
2021-04-02 14:18:40 +0200

    Update BookController urls mapping

--- src/main/java/pl/wydzials/medialist/controller/BookController.java
+++ src/main/java/pl/wydzials/medialist/controller/BookController.java
@@ -2,14 +2,12 @@ package pl.wydzials.medialist.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.wydzials.medialist.model.Book;
 import pl.wydzials.medialist.repository.BookRepository;
 
 @Controller
+@RequestMapping("/user/books")
 public class BookController {
     private final BookRepository bookRepository;
 
@@ -17,26 +15,26 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
-    @GetMapping("/books")
+    @GetMapping()
     public String books(Model model) {
         model.addAttribute("books", bookRepository.findAllByOrderByPriorityDesc());
         return "books";
     }
 
-    @GetMapping("/books/add")
+    @GetMapping("/add")
     public String addBookGet(Model model) {
         return "add_book";
     }
 
-    @PostMapping("/books/add")
+    @PostMapping("/add")
     public String addBookPost(@ModelAttribute Book book) {
         bookRepository.save(book);
-        return "redirect:/books";
+        return "redirect:/user/books";
     }
 
-    @GetMapping("/books/delete/{id}")
+    @GetMapping("/delete/{id}")
     public String deleteBook(@PathVariable long id) {
         bookRepository.deleteById(id);
-        return "redirect:/books";
+        return "redirect:/user/books";
     }
 }
